8cc0e69e7f184f4294b1c3d985f87390b5784670
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2016-02-21 11:57:18 -0800

    Added possibility to cache results and compute hashes

--- datapath/core.py
+++ datapath/core.py
@@ -1,24 +1,30 @@
 import os
 import dask.threaded
+import dis
+import dill
+import joblib
+import marshal
+
 from dask.compatibility import apply
-import cPickle as pickle
+
 
 class DataCache:
-    
+
     def __init__(self, directory='./dc'):
         if not os.path.exists(directory):
             os.mkdir(directory)
-        
+
         self.directory = directory
         self.graph = {}
+        self.step_graph = {}
         self.observed = []
 
     def step(self, target, *args, **kwargs):
         name = kwargs.get('name')
-        
+
         if name is None:
             name = target.__name__
-        
+
         return Step(self, name, target, args, kwargs)
 
     def record(self, target, *args, **kwargs):
@@ -26,65 +32,102 @@ class DataCache:
         for arg in args:
             if isinstance(arg, Step):
                 arg = arg.get()
-            
+
             processed_args.append(arg)
         result = target(*processed_args, **kwargs)
-        
+
         self.observed.append((target.__name__, args, kwargs, result))
         return result
-        
+
     def summary(self):
         # We need to infer the pipeline from our graph.
         lines = []
         for name, args, kwargs, result in self.observed:
-            args = tuple(a if not isinstance(a, Step) else a.name for a in args)
-            
+            args = tuple(a if not isinstance(a, Step)
+                         else a.name for a in args)
+
             lines.append(name + str(args) + ' = ' + str(result))
-        
+
         return '\n'.join(lines)
 
     def store(self, name, value):
-        return pickle.dump(value, open(os.path.join(self.directory, name), 'wb'))
-    
+        return joblib.dump(value, os.path.join(self.directory, name))
+
     def load(self, name):
-        return pickle.load(open(os.path.join(self.directory, name), 'rb'))
+        return joblib.load(os.path.join(self.directory, name))
+
+    def load_hash(self, name):
+        hash_file = os.path.join(self.directory, name + '.hash')
+        if not os.path.exists(hash_file):
+            return None
+        else:
+            return open(hash_file).read()
+
+    def store_hash(self, name, hash):
+        hash_file = os.path.join(self.directory, name + '.hash')
+        with open(hash_file, 'w') as fd:
+            fd.write(hash)
+
 
 class Step:
-    
+
     def __init__(self, dc, name, target, args, kwargs):
         self.dc = dc
         self.target = target
         self.name = name
         self.args = args
         self.kwargs = kwargs
-        
+
         # Create a new entry in the graph
-        self.dc.graph[self.name] = (apply_with_kwargs, self.target, list(args), list(kwargs))
+        self.recompute()
+        self.dc.step_graph[self.name] = self
+
+    def recompute(self):
+        self.dc.graph[self.name] = (apply_with_kwargs, self.target,
+                                    list(self.args), list(self.kwargs))
 
     def step(self, target, *args, **kwargs):
-        
+
         name = kwargs.get('name')
         if name is None:
             name = target.__name__
-        
+
         # We prepend our result as our first argument to allow easy chaining
         args = (self.name,) + args
-        
+
         return Step(self.dc, self.name + '->' + name, target, args, kwargs)
-    
+
     def get(self):
         result = dask.threaded.get(self.dc.graph, self.name)
         return result
 
     def checkpoint(self):
-        # We write stuff to disk
-        result = self.get()
-        self.dc.store(self.name + '.store', result)
-        
+        hash_ = self.hash()
+        if hash_ != self.dc.load_hash(self.name):
+            # If hash has changed, we write stuff to disk
+            self.recompute()
+            result = self.get()
+            self.dc.store(self.name + '.store', result)
+            self.dc.store_hash(self.name, hash_)
+
         # We replace the calculation with the cached value
         self.dc.graph[self.name] = (self.dc.load, self.name + '.store')
         return self
-        
+
+    def previous(self):
+        return self.dc.step_graph['->'.join(self.name.split('->')[:-1])]
+
+    def hash(self):
+        uniquity = (self.name, self.args, self.kwargs,
+                    marshal.dumps(self.target.__code__))
+
+        if '->' in self.name:
+            previous_hash = self.previous().hash()
+        else:
+            previous_hash = ''
+
+        return previous_hash + joblib.hash(uniquity)
+
+
 def apply_with_kwargs(function, args, kwargs):
     return function(*args, **dict(kwargs))
-    
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,39 +1,105 @@
 import numpy as np
+import os
+
 from datapath.core import DataCache
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LinearRegression
 
-def test_hello():
-    
-    dc = DataCache('./dc')
-    
-    # TODO: how to parametrize this kind of stuff?
-    def make_data_set():
-        X = np.arange(20)[np.newaxis].T
-        y = np.arange(20)
-        return X, y
-    
-    def split(prev):
-        X, y = prev
-        return train_test_split(X, y)
-        
-    def square(prev):
-        X, y = prev
-        
-        return X**2, y
 
-    def linear_fit(dataset, C=1.0):
-        X_train, X_test, y_train, y_test = dataset
-        lr = LinearRegression(C)
-        lr.fit(X_train, y_train)
-        
-        return lr, lr.score(X_test, y_test)
-    
-    dataset = dc.step(make_data_set).step(split).checkpoint()
-    
-    lr, score = dc.record(linear_fit, dataset)
-    
-    dataset2 = dc.step(make_data_set).step(square).step(split).checkpoint()
-    lr, score = dc.record(linear_fit, dataset2)
-    
-    print dc.summary()
+def make_data_set():
+    X = np.arange(20)[np.newaxis].T
+    y = np.arange(20)
+    return X, y
+
+
+def split(prev):
+    X, y = prev
+    return train_test_split(X, y)
+
+
+def square(prev):
+    X, y = prev
+
+    return X**2, y
+
+
+def linear_fit(dataset, C=1.0):
+    X_train, X_test, y_train, y_test = dataset
+    lr = LinearRegression(C)
+    lr.fit(X_train, y_train)
+
+    return lr, lr.score(X_test, y_test)
+
+import py
+
+
+@py.test.fixture()
+def datacache(request):
+    dc = DataCache('./dc')
+
+    def teardown():
+        py.path.local('./dc').remove()
+    request.addfinalizer(teardown)
+    return dc
+
+
+def test_example1(datacache):
+    # TODO: how to parametrize this kind of stuff?
+    dataset = datacache.step(make_data_set).step(split).checkpoint()
+
+    lr, score = datacache.record(linear_fit, dataset)
+
+    dataset2 = datacache.step(make_data_set).step(
+        square).step(split).checkpoint()
+    lr, score = datacache.record(linear_fit, dataset2)
+
+    # We need to check if hash was created
+    assert os.path.exists('./dc/' + dataset.name + '.hash')
+
+
+def test_hash_changed(datacache):
+    global make_data_set
+    make_data_set_old = make_data_set
+
+    hash_1 = datacache.step(make_data_set).step(split).hash()
+    hash_2 = datacache.step(make_data_set).step(square).step(split).hash()
+    hash_3 = datacache.step(make_data_set).hash()
+
+    assert hash_1 != hash_2
+
+    # A function was rewritten in a different way
+    def make_data_set():
+        X = np.arange(10)[np.newaxis].T
+        y = np.arange(10)
+        return X, y
+
+    print(make_data_set.__code__)
+
+    # A function was rewritten
+    hash_4 = datacache.step(make_data_set).hash()
+    assert hash_3 != hash_4
+
+    # The code of a dependency changed, we're supposed to
+    # update our cache
+    hash_5 = datacache.step(make_data_set).step(split).hash()
+    assert hash_1 != hash_5
+
+    make_data_set = make_data_set_old
+
+
+def test_cache_func_changed(datacache):
+    global make_data_set
+    make_data_set_old = make_data_set
+    step = datacache.step(make_data_set).step(square).checkpoint()
+    step_x = step.get()[0]
+
+    # A function was rewritten in a different way
+    def make_data_set():
+        X = np.arange(10)[np.newaxis].T
+        y = np.arange(10)
+        return X, y
+
+    step_mod = datacache.step(make_data_set).step(square).checkpoint()
+    step_mod_x = step_mod.get()[0]
+    assert not np.array_equal(step_x, step_mod_x)
+    make_data_set = make_data_set_old
